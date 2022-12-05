74e5bfda0b77aa3c5e8113100b2f5f543266731d
Marina Liubas <u_popa_bila_@inbox.ru>
2022-12-05 20:31:07 +0300

    Arrays - homework completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  
+    return arr1.length === arr2.length && arr1.every((item, i) => item === arr2[i])
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  return users.filter(user => user.gender === gender).reduce((acc, user, i, users) => {
+    return i === users.length - 1 ? (acc + user.age) / users.length : acc + user.age;
+    }, 0)
 }
