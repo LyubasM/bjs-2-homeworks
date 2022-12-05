514fea9099769f9ee212cc23bf952d1b3660a20c
Marina Liubas <u_popa_bila_@inbox.ru>
2022-12-05 12:09:45 +0300

    Functions - according to comments edits have been made

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -23,55 +23,55 @@ function getArrayParams(...arr) {
 function summElementsWorker(...arr) {
   if (arr.length === 0) {
     return 0;
-  } else {
-    let sum = arr.reduce((sum, current) => sum + current, 0);
-    return sum;
   }
+  
+  let sum = arr.reduce((sum, current) => sum + current, 0);
+    return sum;
 }
 
 function differenceMaxMinWorker(...arr) {
   if (arr.length === 0) {
     return 0;
-  } else {
-    return Math.max(...arr) - Math.min(...arr);
-  }
+  } 
+  
+  return Math.max(...arr) - Math.min(...arr);
 }
 
 function differenceEvenOddWorker(...arr) {
   if (arr.length === 0) {
     return 0;
-  } else {  
-    let sumEvenElement = 0;
-    let sumOddElement = 0;
+  }
 
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] % 2 === 0) {
-        sumEvenElement +=arr[i];
-      } else {
-        sumOddElement +=arr[i];
-      }
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement +=arr[i];
+    } else {
+      sumOddElement +=arr[i];
     }
+  }
 
     return sumEvenElement - sumOddElement;
-  }
 }
 
 function averageEvenElementsWorker(...arr) {
   if (arr.length === 0) {
     return 0;
-  } else {
-    let sumEvenElement = 0;
-    let countEvenElement = 0;
+  }
 
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] % 2 === 0) {
-        sumEvenElement +=arr[i];
-        countEvenElement++;
-      }
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 === 0) {
+      sumEvenElement +=arr[i];
+      countEvenElement++;
     }
+  }
 
     return sumEvenElement / countEvenElement;
-  }
 }
 
 function makeWork (arrOfArr, func) {
@@ -85,5 +85,4 @@ function makeWork (arrOfArr, func) {
   }
 
   return maxWorkerResult;
-
 }
